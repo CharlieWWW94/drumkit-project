d3ee6b5d1e25ba7b4745b16ab54bb8464fe610e5
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-10-06 16:52:12 +0100

    Add responsiveness and play on click functionality to drumkit

---
+++ script.js
@@ -0,0 +1,30 @@
+const soundPaths = {
+    2: "./static/audio/ride.wav",
+    3: "./static/audio/floor-tom.wav",
+    4: "./static/audio/high-tom.wav",
+    5: "./static/audio/crash.wav",
+    6: "./static/audio/hi-hat.wav",
+    8: "./static/audio/medium-tom.wav",
+    10: "./static/audio/bass-drum.wav",
+    11: "./static/audio/snare.wav",
+    15: "./static/audio/bass-drum.wav",
+    16: "./static/audio/bass-drum.wav",
+};
+
+const playSound = (sound) => {
+    const sounds = Object.keys(soundPaths);
+    if (sounds.includes(sound)) {
+        let drum = new Audio();
+        drum.src = soundPaths[sound];
+        drum.play();
+    };
+};
+
+const drumZones = document.querySelectorAll(".pad")
+
+drumZones.forEach((drumZone) => {
+    drumZone.addEventListener("click", () => {
+        playSound(drumZone.id)
+    })
+}
+)
