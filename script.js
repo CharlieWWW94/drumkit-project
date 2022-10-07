ab82da538d8548da5df02c007805a9b9926cf316
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-10-07 11:52:52 +0100

    Add Nautilus audio pad

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
 //File paths for audio files
 const soundPaths = {
-    2: "./static/audio/ride.wav",
-    3: "./static/audio/floor-tom.wav",
-    4: "./static/audio/high-tom.wav",
-    5: "./static/audio/crash.wav",
-    6: "./static/audio/hi-hat.wav",
-    8: "./static/audio/medium-tom.wav",
-    10: "./static/audio/bass-drum.wav",
-    11: "./static/audio/snare.wav",
-    15: "./static/audio/bass-drum.wav",
-    16: "./static/audio/bass-drum.wav",
+  2: "ride",
+  3: "floor-tom",
+  4: "high-tom",
+  5: "crash",
+  6: "hi-hat",
+  8: "medium-tom",
+  10: "bass-drum",
+  11: "snare",
+  15: "bass-drum",
+  16: "bass-drum",
 };
 
-const drumParent = document.querySelector(".drum-div")
+const padSwitch = document.querySelector(".switch");
+let naut = false;
+const drumParent = document.querySelector(".drum-div");
+const heading = document.querySelector("#title");
 
-//Populate page with drumpad grid elements 
+//Populate page with drumpad grid elements
 for (let i = 1; i <= 18; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "pad";
-    newDiv.id = `${i}`;
-    drumParent.appendChild(newDiv);
+  const newDiv = document.createElement("div");
+  newDiv.className = "pad";
+  newDiv.id = `${i}`;
+  drumParent.appendChild(newDiv);
 }
 
 //Reproduces sound on click
 const playSound = (sound) => {
-    const sounds = Object.keys(soundPaths);
-    if (sounds.includes(sound)) {
-        let drum = new Audio();
-        drum.src = soundPaths[sound];
-        drum.play();
-    };
+  const sounds = Object.keys(soundPaths);
+  if (sounds.includes(sound)) {
+    let drum = new Audio();
+    drum.src = naut ? `./static/audio/nautilus/${soundPaths[sound]}.wav` : `./static/audio/${soundPaths[sound]}.wav`
+    drum.play();
+  }
 };
 
 //Cycles through each pad, adding event listener by id
-const drumZones = document.querySelectorAll(".pad")
+const drumZones = document.querySelectorAll(".pad");
 drumZones.forEach((drumZone) => {
-    drumZone.addEventListener("click", () => {
-        playSound(drumZone.id)
-    })
-}
-)
+  drumZone.addEventListener("click", () => {
+    playSound(drumZone.id);
+  });
+});
+
+//use a selected class to do this.
+
+padSwitch.addEventListener("click", () => {
+    if (naut === false) {
+    naut = true;
+    heading.innerHTML = "Nautilus";
+    heading.style.color = "violet";
+    drumParent.style.boxShadow = "0px 5px 40px -4px  blueviolet"
+    drumParent.style.backgroundImage = "url('./static/images/etherscape.jpg')";
+    padSwitch.innerHTML = "KraterPad"
+    padSwitch.style.boxShadow = "none";
+    padSwitch.style.backgroundColor = "orangered";
+    } else {
+        naut = false;
+        heading.innerHTML = "KraterPad";
+        heading.style.color = "orangered";
+        drumParent.style.boxShadow = "0px 5px 40px -4px  green"
+        drumParent.style.backgroundImage = "url('./static/images/drumkit.png')";
+        padSwitch.innerHTML = "Nautilus";
+        padSwitch.style.boxShadow = "0px 5px 40px -4px  blueviolet";
+        padSwitch.style.backgroundColor = "rgb(24, 24, 24)";
+    }
+});
